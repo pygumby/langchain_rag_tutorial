b12ef6521e7775445f4155f556012d5455c8c0e8
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2025-06-17 21:29:39 +0000

    Add section "Indexing – Loading documents" of "Part 1"

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from langchain_openai import OpenAIEmbeddings
 # Vector store
 from langchain_core.vectorstores import InMemoryVectorStore
 
+# Indexing – Loading documents
+from langchain_community.document_loaders import WebBaseLoader
+import bs4
+
 if not os.environ.get("GOOGLE_API_KEY"):
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
 
@@ -21,3 +25,13 @@ embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 vector_store = InMemoryVectorStore(embeddings)
 
 # Load and chunk contents of the blog
+bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs4_strainer},
+)
+docs = loader.load()
+
+assert len(docs) == 1  # Expecting exactly one document to be loaded
+print(f"Total characters: {len(docs[0].page_content)}")
+print(docs[0].page_content[:500])
