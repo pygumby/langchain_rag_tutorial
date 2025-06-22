5097f28c77e7a72ca402a928ecae3f9dfc011952
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2025-06-22 15:01:30 +0000

    Add section "Retrieval and generation" of "Part 1"

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""LangChain RAG tutorial"""
+
 import getpass
 import os
 
@@ -17,6 +19,17 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 # Indexing - Storing documents
 from langchain_core.vectorstores import InMemoryVectorStore
 
+# Retrieval and generation
+from langchain import hub
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+from langgraph.graph import START, StateGraph
+
+
+os.environ["LANGSMITH_TRACING"] = "true"
+
+if not os.environ.get("LANGSMITH_API_KEY"):
+    os.environ["LANGSMITH_API_KEY"] = getpass.getpass("Enter API key for LangSmith: ")
 
 if not os.environ.get("OPENAI_API_KEY"):
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
@@ -49,3 +62,61 @@ print(f"Split blog post into {len(all_splits)} sub-documents.")
 # Embed and store chunks
 document_ids = vector_store.add_documents(documents=all_splits)
 # print(document_ids[:3])
+
+# Retrieve and generate
+prompt = hub.pull("rlm/rag-prompt")
+
+# example_message = prompt.invoke(
+#     {
+#         "context": "(context goes here)",
+#         "question": "(question goes here)",
+#     }
+# ).to_messages()
+
+# assert len(example_message) == 1
+# print(example_message[0].content)
+
+
+class State(TypedDict):
+    """State of the RAG system"""
+
+    question: str
+    context: List[Document]
+    answer: str
+
+
+def retrieve(state: State):
+    """Retrieval step"""
+
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    """Generation step"""
+
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke(
+        {
+            "question": state["question"],
+            "context": docs_content,
+        }
+    )
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+# result = graph.invoke({"question": "What is task decomposition?"})
+
+# print(f"Context: {result["context"]}\n\n")
+# print(f"Answer: {result["answer"]}")
+
+for message, metadata in graph.stream(
+    {"question": "What is task decomposition?"}, stream_mode="messages"
+):
+    print(message.content, end="")
+print()
